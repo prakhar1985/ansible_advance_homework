a5cc6243eb25df4cde546f42b87612ef5c1b6390
root <root@bastion.ba5c.example.opentlc.com>
2020-05-21 18:08:40 +0000

    update

--- grade.sh
+++ grade.sh
@@ -5,6 +5,24 @@ echo
 echo "Login to tower1, go to inventory and  get guid"
 echo
 read -p "Enter Three Tier App GUID : "  THREE_TIER_GUID
-
 export THREE_TIER_GUID
+
+cat > ~/grade_result.txt <<EOF
+*******************************
+    Grade Result
+*******************************
+Tower's GUID: ${TOWER_GUID}
+OSP's GUID: ${OSP_GUID}
+3Tier App's GUID: ${THREE_TIER_GUID}
+*******************************
+
+EOF
+
 ansible-playbook new-grade-playbook.yml 
+
+
+cat >> ~/grade_result.txt <<EOF
+
+*******************************
+
+EOF
